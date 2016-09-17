a81723e1aef513d6072cea1098c44acdb580e0c5
chernysh2909 <chernysh2909@gmail.com>
2016-09-17 13:12:20 +0300

    Signed-off-by: chernysh2909 <chernysh2909@gmail.com> contennt-card

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,27 @@
 console.log('\'Allo \'Allo!');
-$(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-  	items: 1,
-  	nav: true,
-  	loop:true,
-  });
+$(document).ready(function() {
+    $('#owl-demo').owlCarousel({
+    	singleItem:true,
+    	// navigation:true,
+    	paginationSpeed:800
+    });
+      var owl = $('#owl-demo');
+
+    owl.owlCarousel({
+
+        singleItem: true,
+        // itemsDesktop: [1199, 3],
+        // itemsDesktopSmall: [979, 3],
+        pagination: false
+    });
+
+    // Custom Navigation Events
+    $('#pref').click(function() {
+        owl.trigger('owl.next');
+    })
+    $('#next').click(function() {
+        owl.trigger('owl.prev');
+    })
 });
+
+
